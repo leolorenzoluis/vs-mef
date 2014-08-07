7711457851ccce38a596ed9261bb68cd729935db
James Williams <jamew@microsoft.com>
2014-08-07 16:32:37 -0700

    Left IPartDiscovery out of the change, adding now

---
+++ Microsoft.VisualStudio.Composition/IPartDiscovery.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Microsoft.VisualStudio.Composition
+{
+    public interface IPartDiscovery
+    {
+        DiscoveredParts CreateParts(IEnumerable<Type> types);
+        DiscoveredParts CreateParts(IEnumerable<Assembly> assemblies);        
+    }
+}
