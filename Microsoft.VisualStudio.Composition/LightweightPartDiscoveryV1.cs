f36bcb142657ece1299d629674b6a379cec08154
James Williams <jamew@microsoft.com>
2014-08-15 18:40:25 -0700

    Pushing with changes to catalog publishing that should fix AcquireSingleExport and AcquireExportWIthImport

--- Microsoft.VisualStudio.Composition/LightweightPartDiscoveryV1.cs
+++ Microsoft.VisualStudio.Composition/LightweightPartDiscoveryV1.cs
@@ -7,6 +7,7 @@
     using System.Linq;
     using System.Reflection;
     using System.Reflection.Metadata;
+    using System.Reflection.Metadata.Decoding;
     using System.Reflection.Metadata.Ecma335;
     using System.Text;
     using System.Threading.Tasks;
@@ -16,35 +17,13 @@
         public override ComposablePartDefinition CreatePart(MetadataReader metadataReader, TypeDefinition typeDefinition,
             HashSet<string> knownExportTypes, string assemblyName, int metadataToken)
         {
+            SignatureTypeProvider decoder = new SignatureTypeProvider(metadataReader);
             ComposablePartDefinition toReturn = null;
             var attributes = typeDefinition.GetCustomAttributes().Select(metadataReader.GetCustomAttribute);
 
             foreach (var attr in attributes)
             {
-                var handle = metadataReader.GetAttributeTypeHandle(attr);
-                var handleType = handle.HandleType;
-                string name = string.Empty;
-
-                if (handleType == HandleType.Type)
-                {
-                    var td = metadataReader.GetTypeDefinition((TypeHandle)handle);
-                    name = metadataReader.GetString(td.Namespace) ?? string.Empty;
-                    if (!string.IsNullOrEmpty(name))
-                    {
-                        name = name + ".";
-                    }
-                    name = name + metadataReader.GetString(td.Name);
-                }
-                else if (handleType == HandleType.TypeReference)
-                {
-                    var tr = metadataReader.GetTypeReference((TypeReferenceHandle)handle);
-                    name = metadataReader.GetString(tr.Namespace) ?? string.Empty;
-                    if (!string.IsNullOrEmpty(name))
-                    {
-                        name = name + ".";
-                    }
-                    name = name + metadataReader.GetString(tr.Name);
-                }
+                var name = UtilityFunctions.GetAttributeTypeName(attr, metadataReader);
 
                 if (knownExportTypes.Contains(name))
                 {
@@ -55,13 +34,65 @@
                         var typeIdentity = ContractNameServices.GetTypeIdentity(typeDefinition, metadataReader);
 
                         metadata.Add("ExportTypeIdentity", typeIdentity);
-                        metadata.Add("System.ComponentModel.Composition.CreationPolicy", "NonShared");
+                        metadata.Add("System.ComponentModel.Composition.CreationPolicy", CreationPolicy.NonShared);
                         
                         exports.Add(new ExportDefinition(ContractNameServices.GetTypeIdentity(typeDefinition, metadataReader), metadata));
 
                         var typeRefArray = ImmutableArray.CreateBuilder<TypeRef>();
+
+                        var fieldHandles = typeDefinition.GetFields();
+                        var propertyHandles = typeDefinition.GetProperties();
+
                         var exportingMembers = ImmutableDictionary.CreateBuilder<MemberRef, IReadOnlyCollection<ExportDefinition>>();
+                        
                         var importingMembers = ImmutableArray.CreateBuilder<ImportDefinitionBinding>();
+                        foreach (var propHandle in propertyHandles)
+                        {
+                            var prop = metadataReader.GetProperty(propHandle);
+                            foreach (var custAttr in prop.GetCustomAttributes().Select(metadataReader.GetCustomAttribute))
+                            {
+                                var attrType = UtilityFunctions.GetAttributeTypeName(custAttr, metadataReader);
+                                if (attrType == "System.ComponentModel.Composition.ImportAttribute")
+                                {
+                                    var methodHandles = prop.GetAssociatedMethods();
+                                    var accessor = metadataReader.GetMethod(methodHandles.Setter);
+                                    var sig = SignatureDecoder.DecodeMethodSignature(accessor.Signature, decoder);
+                                    var importingTypeHandle = (TypeHandle)sig.ParameterTypes.First();
+                                    var importingTypeDef = metadataReader.GetTypeDefinition(importingTypeHandle);
+                                    var importTypeName = ContractNameServices.GetTypeIdentity(importingTypeDef, metadataReader);
+                                    var token = metadataReader.GetToken(importingTypeHandle);
+                                    var importMetadata = ImmutableDictionary.CreateBuilder<string, object>();
+                                    var additionalConstraints = ImmutableArray.CreateBuilder<IImportSatisfiabilityConstraint>();
+                                    additionalConstraints.Add(new ExportTypeIdentityConstraint(importTypeName));
+
+                                    var typeRef = Reflection.TypeRef.Get(
+                                    new AssemblyName(assemblyName),
+                                    metadataToken,
+                                    false,
+                                    0,
+                                    typeRefArray.ToImmutable());
+                                    var propertyRef = new PropertyRef(
+                                        typeRef,
+                                        metadataReader.GetToken(propHandle),
+                                        metadataReader.GetToken(methodHandles.Getter),
+                                        metadataReader.GetToken(methodHandles.Setter));
+                                    var memberRef = new MemberRef(propertyRef);
+                                    var importDefinition = new ImportDefinition(
+                                        importTypeName,
+                                        ImportCardinality.ExactlyOne,
+                                        importMetadata.ToImmutable(),
+                                        additionalConstraints.ToImmutable());
+                                    
+                                    var importDefinitionBinding = new ImportDefinitionBinding(
+                                        importDefinition,
+                                        typeRef,
+                                        memberRef);
+                                    importingMembers.Add(importDefinitionBinding);
+                                    break;
+                                }
+                            }
+                        }
+                        
                         var importingConstructors = ImmutableArray.CreateBuilder<ImportDefinitionBinding>();
 
                         toReturn = new ComposablePartDefinition(
@@ -83,6 +114,90 @@
             }
             
             return toReturn;
+        }
+
+        private class SignatureTypeProvider : ISignatureTypeProvider<object>
+        {
+            private MetadataReader _reader;
+            public SignatureTypeProvider(MetadataReader reader)
+            {
+                _reader = reader;
+            }
+            public object GetFunctionPointerType(MethodSignature<object> signature)
+            {
+                throw new NotImplementedException();
+            }
+
+            public object GetGenericMethodParameter(int index)
+            {
+                throw new NotImplementedException();
+            }
+
+            public object GetGenericTypeParameter(int index)
+            {
+                throw new NotImplementedException();
+            }
+
+            public object GetModifiedType(object unmodifiedType, ImmutableArray<CustomModifier<object>> customModifiers)
+            {
+                throw new NotImplementedException();
+            }
+
+            public object GetPinnedType(object elementType)
+            {
+                throw new NotImplementedException();
+            }
+
+            public object GetPrimitiveType(PrimitiveTypeCode typeCode)
+            {
+                switch (typeCode)
+                {
+                    case PrimitiveTypeCode.Void:
+                        return "void";
+                    default:
+                        return "unknown type";
+                }
+            }
+
+            public object GetTypeFromDefinition(TypeHandle handle)
+            {
+                return handle;
+            }
+
+            public object GetTypeFromReference(TypeReferenceHandle handle)
+            {
+                throw new NotImplementedException();
+            }
+
+            public MetadataReader Reader
+            {
+                get { return _reader; }
+            }
+
+            public object GetArrayType(object elementType, ArrayShape shape)
+            {
+                throw new NotImplementedException();
+            }
+
+            public object GetByReferenceType(object elementType)
+            {
+                throw new NotImplementedException();
+            }
+
+            public object GetGenericInstance(object genericType, ImmutableArray<object> typeArguments)
+            {
+                throw new NotImplementedException();
+            }
+
+            public object GetPointerType(object elementType)
+            {
+                throw new NotImplementedException();
+            }
+
+            public object GetSZArrayType(object elementType)
+            {
+                throw new NotImplementedException();
+            }
         }
     }
 }
---
+++ Microsoft.VisualStudio.Composition/UtilityFunctions.cs
@@ -0,0 +1,45 @@
+﻿namespace Microsoft.VisualStudio.Composition
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Reflection;
+    using System.Reflection.Metadata;
+    using System.Text;
+    using System.Threading.Tasks;
+    using Validation;
+
+    internal static class UtilityFunctions
+    {
+        public static string GetAttributeTypeName(CustomAttribute attribute, MetadataReader metadataReader)
+        {
+            var handle = metadataReader.GetAttributeTypeHandle(attribute);
+            var handleType = handle.HandleType;
+            
+            string name = string.Empty;
+
+            if (handleType == HandleType.Type)
+            {
+                var td = metadataReader.GetTypeDefinition((TypeHandle)handle);
+                name = metadataReader.GetString(td.Namespace) ?? string.Empty;
+                if (!string.IsNullOrEmpty(name))
+                {
+                    name = name + ".";
+                }
+                name = name + metadataReader.GetString(td.Name);
+            }
+            else if (handleType == HandleType.TypeReference)
+            {
+                var tr = metadataReader.GetTypeReference((TypeReferenceHandle)handle);
+                name = metadataReader.GetString(tr.Namespace) ?? string.Empty;
+                if (!string.IsNullOrEmpty(name))
+                {
+                    name = name + ".";
+                }
+                name = name + metadataReader.GetString(tr.Name);
+            }
+
+            return name;
+        }
+    }
+}
